fe4e8dc0df6a976980e2be21991689dec71cb96d
ChuckleScriptCraft <hxzzmxnch@gmail.com>
2023-12-29 22:24:09 +0000

    Broken feature needs more cpu

--- 14/aoc14.r
+++ 14/aoc14.r
@@ -46,45 +46,74 @@ adjusted_roll <- roll_possible[pmax(1, blind_roll)] +
 
 
 # Part 2 ------------------------------------------------------------------
-rotate <- function(x) t(apply(x, 2, rev))
 
+# Part 2 ------------------------------------------------------------------
+rotate <- function(x) t(apply(x, 2, rev))
+empty_lit = list()
 
 
 counter = 0
 mat <- as.matrix(df)
 
+#Find break
+
 repeat{
   
-for(i in 1:ncol(df)){
-
-  col1 <- mat[,i]
-  
-  round <- which(col1 == "O") 
-  square <- which(col1 == "#") + 1
-  roll_possible <- unique(c(1,square))
-  
-  # Roll desintation 
-  blind_roll <- findInterval(round, roll_possible)
-  
-  adjusted_roll <- roll_possible[pmax(1, blind_roll)] + 
-    ave(seq_along(ave(seq_along(blind_roll))), blind_roll, FUN = function(x) seq_along(x) - 1)
-  
- col1[col1 == "O"] <- "."
- col1[adjusted_roll] <- "O" 
- 
- mat[,i] <- col1
-}
+  for(i in 1:ncol(df)){
+    
+    col1 <- mat[,i]
+    
+    round <- which(col1 == "O") 
+    square <- which(col1 == "#") + 1
+    roll_possible <- unique(c(1,square))
+    
+    # Roll desintation 
+    blind_roll <- findInterval(round, roll_possible)
+    
+    adjusted_roll <- roll_possible[pmax(1, blind_roll)] + 
+      ave(seq_along(ave(seq_along(blind_roll))), blind_roll, FUN = function(x) seq_along(x) - 1)
+    
+    col1[col1 == "O"] <- "."
+    col1[adjusted_roll] <- "O" 
+    
+    mat[,i] <- col1
+  }
   counter = counter + 1
   
-  if(counter == 1000000000*4){
-    break
+  if(counter %% 4){
+    empty_lit <- c(empty_lit, list(mat))
+    
+    predefined_matrix <- empty_lit[[1]]
+    
+    is_equal <- sapply(empty_lit, function(mat) identical(mat, predefined_matrix))
+    
+    if(sum(is_equal) > 1){
+      print(counter)
+      break
+    }
+    
   }
   
+  
+  
+  
+  
+  if(counter == 4000000000){
+    break
+  }
   mat <- rotate(mat)
-
+  
   
 }
 
-# Part 2 Answer
 
-rowSums(mat == "O") * (101 - seq_along(rowSums(mat == "O")))
+
+predefined_matrix <- empty_lit[[3]]
+
+
+# Check which matrices in the list are equal to the predefined matrix
+is_equal <- sapply(empty_lit, function(mat) identical(mat, predefined_matrix))
+
+which(is_equal == TRUE)
+sum(is_equal)
+
